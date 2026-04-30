71fa08447f903132a96484aa06f09090547a708b
sioti <164847212+Siotii@users.noreply.github.com>
2026-04-30 21:14:17 +0400

    Add files via upload

---
+++ database.py
@@ -0,0 +1,60 @@
+import sqlite3
+import hashlib
+
+DB_NAME = "padflock.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME)
+
+
+def init_db():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def register_user(username, password):
+    conn = connect()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, hash_password(password))
+        )
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conn.close()
+
+
+def login_user(username, password):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, hash_password(password))
+    )
+
+    user = cursor.fetchone()
+    conn.close()
+
+    return user is not None
